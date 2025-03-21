5b5d12120d74efcccba9d2c6185d55b34852e7dd
Flabb <dhimasyulianto440@gmail.com>
2025-03-21 22:19:30 +0700

    add : crud

---
+++ app/views/templates/footer.php
@@ -0,0 +1,8 @@
+</div>
+<footer class="bg-dark text-light text-center py-3 mt-4">
+    <p>&copy; <?= date("Y") ?> CRUD Produk. All rights reserved.</p>
+</footer>
+<script src="/produk/assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
